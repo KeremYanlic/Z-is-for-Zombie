cd295688b7f9ef656f63ede23b9691a32dc051ac
KeremYanlic <keremyanlc@outlook.com>
2023-09-07 23:54:00 +0300

    PlayerController script file has been updated

---
+++ Player/PlayerController.cs
@@ -0,0 +1,436 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Player))]
+[RequireComponent(typeof(PlayerStatus))]
+[DisallowMultipleComponent]
+public class PlayerController : MonoBehaviour
+{
+    #region Tooltip
+    [Tooltip("MovementDetailsSO scriptable object containing movement details such as speed")]
+    #endregion Tooltip
+    [SerializeField] private MovementDetailSO movementDetails;
+
+    private Player player;
+    private PlayerStatus playerStatus;
+    private bool leftMouseDownPreviousFrame = false;
+    private int currentWeaponIndex = 1;
+    private float movementSpeed;
+    private float walkSpeed;
+    private float runSpeed;
+    private Coroutine playerRollCoroutine;
+    private WaitForFixedUpdate waitForFixedUpdate;
+    private float playerRollCooldownTimer = 0f;
+    private bool isPlayerMovementDisabled = false;
+
+    [HideInInspector] public bool isPlayerRolling = false;
+
+    [SerializeField] private int damageAmount = 10;
+    private void Awake()
+    {
+        // Load components
+        player = GetComponent<Player>();
+        playerStatus = GetComponent<PlayerStatus>();
+        walkSpeed = movementDetails.GetMoveSpeed();
+        runSpeed = movementDetails.GetRunSpeed();
+    }
+
+    private void Start()
+    {
+        // Create waitforfixed update for use in coroutine
+        waitForFixedUpdate = new WaitForFixedUpdate();
+
+        SetStartingWeapon();
+    }
+
+
+    private void Update()
+    {
+        // if player movement disabled then return
+        if (isPlayerMovementDisabled)
+            return;
+
+        // if player is rolling then return
+        if (isPlayerRolling) return;
+
+        // Process the player movement input
+        MovementInput();
+
+        // Process the player weapon input
+        WeaponInput();
+
+
+        // Player roll cooldown timer
+        PlayerRollCooldownTimer();
+
+        // Set player animation based on its movement situation
+        SetPlayerAnimationSpeed();
+    }
+    /// <summary>
+    /// Set the player starting weapon
+    /// </summary>
+    public void SetStartingWeapon()
+    {
+        int index = 1;
+
+        foreach (Weapon weapon in player.weaponList)
+        {
+            if (weapon.weaponDetails == player.playerDetails.startingWeapon)
+            {
+                SetWeaponByIndex(index);
+                break;
+            }
+            index++;
+        }
+    }
+
+    /// <summary>
+    /// Set player animator speed to match movement speed
+    /// </summary>
+    private void SetPlayerAnimationSpeed()
+    {
+        // Set animator speed to match movement speed
+        player.animator.speed = movementSpeed / Settings.baseSpeedForPlayerAnimations;
+    }
+
+    /// <summary>
+    /// Player movement input
+    /// </summary>
+    private void MovementInput()
+    {
+        // Get movement input
+        float horizontalMovement = Input.GetAxisRaw("Horizontal");
+        float verticalMovement = Input.GetAxisRaw("Vertical");
+        bool isLeftCtrlClicked = Input.GetKeyDown(KeyCode.LeftControl);
+        bool isLeftShiftBeingClicked = Input.GetKey(KeyCode.LeftShift);
+
+        if (isLeftShiftBeingClicked && playerStatus.GetCurrentStamina() > 0f)
+        {
+            movementSpeed = runSpeed;
+
+            //Call run event in order to decrease the stamina over time
+            player.runEvent.CallRunEvent();
+        }
+        else
+        {
+            //Call not run event in order to decrease the stamina over time
+            player.runEvent.CallOnNotRun();
+            movementSpeed = walkSpeed;
+        }
+
+        // Create a direction vector based on the input
+        Vector2 direction = new Vector2(horizontalMovement, verticalMovement);
+
+        // Adjust distance for diagonal movement (pythagoras approximation)
+        if (horizontalMovement != 0f && verticalMovement != 0f)
+        {
+            direction *= 0.7f;
+        }
+
+        // If there is movement either move or roll
+        if (direction != Vector2.zero)
+        {
+            if (!isLeftCtrlClicked)
+            {
+                // trigger movement event
+                player.movementByVelocityEvent.CallMovementByVelocityEvent(direction, movementSpeed);
+            }
+            // else player roll if not cooling down
+            else if (playerRollCooldownTimer <= 0f && playerStatus.IsThereStaminaToAction(20))
+            {
+                PlayerRoll((Vector3)direction);
+            }
+        }
+        // else trigger idle event
+        else
+        {
+            player.idleEvent.CallIdleEvent();
+        }
+    }
+
+    /// <summary>
+    /// Player roll
+    /// </summary>
+    private void PlayerRoll(Vector3 direction)
+    {
+        playerRollCoroutine = StartCoroutine(PlayerRollRoutine(direction));
+    }
+
+    /// <summary>
+    /// Player roll coroutine
+    /// </summary>
+    private IEnumerator PlayerRollRoutine(Vector3 direction)
+    {
+        // minDistance used to decide when to exit coroutine loop
+        float minDistance = 0.2f;
+
+        isPlayerRolling = true;
+
+        Vector3 targetPosition = player.transform.position + (Vector3)direction * movementDetails.rollDistance;
+
+        playerStatus.DecreaseStamina(20);
+
+        while (Vector3.Distance(player.transform.position, targetPosition) > minDistance)
+        {
+            player.movementToPositionEvent.CallMovementToPosition(targetPosition, player.transform.position, movementDetails.GetRollSpeed(), direction, isPlayerRolling);
+
+            // yield and wait for fixed update
+            yield return waitForFixedUpdate;
+
+        }
+
+        isPlayerRolling = false;
+
+        // Set cooldown timer
+        playerRollCooldownTimer = movementDetails.rollCooldownTimer;
+
+        player.transform.position = targetPosition;
+    }
+
+    private void PlayerRollCooldownTimer()
+    {
+        if (playerRollCooldownTimer >= 0f)
+        {
+            playerRollCooldownTimer -= Time.deltaTime;
+        }
+    }
+
+    /// <summary>
+    /// Weapon Input
+    /// </summary>
+    private void WeaponInput()
+    {
+        Vector3 weaponDirection;
+        float weaponAngleDegrees, playerAngleDegrees;
+        AimDirection playerAimDirection;
+
+        // Aim weapon input
+        AimWeaponInput(out weaponDirection, out weaponAngleDegrees, out playerAngleDegrees, out playerAimDirection);
+
+        // Fire weapon input
+        FireWeaponInput(weaponDirection, weaponAngleDegrees, playerAngleDegrees, playerAimDirection);
+
+        // Switch weapon input
+        SwitchWeaponInput();
+
+        // Relaod weapon input
+        ReloadWeaponInput();
+
+        //Aim through sight event
+        AimThroughSightInput();
+
+
+    }
+    /// <summary>
+    /// Aim through sight input
+    /// </summary>
+    private void AimThroughSightInput()
+    {
+        int NoScopeGunAimWeight = 2;
+        int NoScopeGunDefaultWeight = 1;
+
+        bool hasGunScope = player.activeWeapon.GetCurrentWeapon().weaponDetails.hasScope;
+
+        if (Input.GetMouseButton(1))
+        {
+            player.aimThroughSightEvent.CallAimThroughSight(NoScopeGunAimWeight, hasGunScope);
+        }
+        else
+        {
+            player.aimThroughSightEvent.CallAimThroughSight(NoScopeGunDefaultWeight, false);
+        }
+
+    }
+
+    private void AimWeaponInput(out Vector3 weaponDirection, out float weaponAngleDegrees, out float playerAngleDegrees, out AimDirection playerAimDirection)
+    {
+        // Get mouse world position
+        Vector3 mouseWorldPosition = UtilsClass.GetWorldMousePosition();
+
+        // Calculate direction vector of mouse cursor from weapon shoot position
+        weaponDirection = (mouseWorldPosition - player.activeWeapon.GetShootPosition());
+
+        // Calculate direction vector of mouse cursor from player transform position
+        Vector3 playerDirection = (mouseWorldPosition - transform.position);
+
+        // Get weapon to cursor angle
+        weaponAngleDegrees = UtilsClass.GetAngleFromMousePosition(weaponDirection);
+
+        // Get player to cursor angle
+        playerAngleDegrees = UtilsClass.GetAngleFromMousePosition(playerDirection);
+
+        // Set player aim direction
+        playerAimDirection = UtilsClass.GetAimDirectionFromAngle(playerAngleDegrees);
+
+        // Trigger weapon aim event
+        player.aimWeaponEvent.CallAimWeaponEvent(playerAimDirection, playerAngleDegrees, weaponAngleDegrees, weaponDirection);
+    }
+
+    private void FireWeaponInput(Vector3 weaponDirection, float weaponAngleDegrees, float playerAngleDegrees, AimDirection playerAimDirection)
+    {
+        // Fire when left mouse button is clicked
+        if (Input.GetMouseButton(0))
+        {
+            // Trigger fire weapon event
+            player.fireWeaponEvent.CallFireWeaponEvent(
+                true,
+                leftMouseDownPreviousFrame,
+                playerAimDirection,
+                playerAngleDegrees,
+                weaponAngleDegrees,
+                weaponDirection
+            );
+            leftMouseDownPreviousFrame = true;
+        }
+
+        else
+        {
+            leftMouseDownPreviousFrame = false;
+        }
+    }
+
+    private void SwitchWeaponInput()
+    {
+        // Switch weapon if mouse scroll wheel selecetd
+        if (Input.mouseScrollDelta.y < 0f)
+        {
+            PreviousWeapon();
+        }
+
+        if (Input.mouseScrollDelta.y > 0f)
+        {
+            NextWeapon();
+        }
+
+        if (Input.GetKeyDown(KeyCode.Alpha1))
+        {
+            SetWeaponByIndex(1);
+        }
+
+        if (Input.GetKeyDown(KeyCode.Alpha2))
+        {
+            SetWeaponByIndex(2);
+        }
+
+        if (Input.GetKeyDown(KeyCode.Alpha3))
+        {
+            SetWeaponByIndex(3);
+        }
+        if (Input.GetKeyDown(KeyCode.Alpha4))
+        {
+            SetWeaponByIndex(4);
+        }
+    }
+
+    private void SetWeaponByIndex(int weaponIndex)
+    {
+        if (weaponIndex - 1 < player.weaponList.Count)
+        {
+            currentWeaponIndex = weaponIndex;
+            player.setActiveWeaponEvent.CallSetActiveWeaponEvent(player.weaponList[weaponIndex - 1]);
+        }
+    }
+
+    private void NextWeapon()
+    {
+        currentWeaponIndex++;
+
+        if (currentWeaponIndex > player.weaponList.Count)
+        {
+            currentWeaponIndex = 1;
+        }
+
+        SetWeaponByIndex(currentWeaponIndex);
+
+    }
+
+    private void PreviousWeapon()
+    {
+        currentWeaponIndex--;
+
+        if (currentWeaponIndex < 1)
+        {
+            currentWeaponIndex = player.weaponList.Count;
+        }
+
+        SetWeaponByIndex(currentWeaponIndex);
+    }
+
+    private void ReloadWeaponInput()
+    {
+        Weapon currentWeapon = player.activeWeapon.GetCurrentWeapon();
+
+        // if current weapon is reloading return
+        if (currentWeapon.isWeaponReloading) return;
+
+        // remaining ammo is less than clip capacity then return and not infinite ammo the return
+        if (
+            currentWeapon.weaponRemainingAmmo < currentWeapon.weaponDetails.weaponClipAmmoCapacity
+
+        ) return;
+
+        // if ammo in clip equals clip capacity then return
+        if (currentWeapon.weaponClipRemainingAmmo == currentWeapon.weaponDetails.weaponClipAmmoCapacity) return;
+
+        if (Input.GetKeyDown(KeyCode.R))
+        {
+            // Call the reload weapon event
+            player.reloadWeaponEvent.CallReloadWeaponEvent(player.activeWeapon.GetCurrentWeapon(), 0);
+        }
+    }
+
+    private void OnCollisionEnter2D(Collision2D collision)
+    {
+        // if collided with something stop player roll coroutine
+        StopPlayerRollRoutine();
+    }
+
+    private void OnCollisionStay2D(Collision2D collision)
+    {
+        // if in collision with something stop player roll coroutine
+        StopPlayerRollRoutine();
+    }
+
+    private void StopPlayerRollRoutine()
+    {
+        if (playerRollCoroutine != null)
+        {
+            StopCoroutine(playerRollCoroutine);
+
+            isPlayerRolling = false;
+        }
+    }
+
+    /// <summary>
+    /// Enable the player movement
+    /// </summary>
+    public void EnablePlayer()
+    {
+        isPlayerMovementDisabled = false;
+    }
+
+    /// <summary>
+    /// Disable the player movement
+    /// </summary>
+    public void DisablePlayer()
+    {
+        transform.position = new Vector3(99999f, 99999f);
+        isPlayerMovementDisabled = true;
+        player.idleEvent.CallIdleEvent();
+    }
+
+
+
+    #region Validation
+#if UNITY_EDITOR
+
+    private void OnValidate()
+    {
+        UtilsClass.ValidateCheckNullValue(this, nameof(movementDetails), movementDetails);
+    }
+
+#endif
+    #endregion Validation
+}
