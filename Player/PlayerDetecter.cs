286fa23313224aa233cf5007e3e6f93147f5d0c2
KeremYanlic <keremyanlc@outlook.com>
2023-09-07 23:55:36 +0300

    PlayerDetecter script file has been updated

---
+++ Player/PlayerDetecter.cs
@@ -0,0 +1,154 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[DisallowMultipleComponent]
+public class PlayerDetecter : MonoBehaviour
+{
+    private Player player;
+    //private RaycastHit2D hit;
+    private GameObject zombie;
+
+    [SerializeField] private LayerMask collidableLayers;
+
+    private Collider2D[] detectedCars = new Collider2D[0];
+    private Collider2D[] detectedZombies = new Collider2D[0];
+
+    private const float X_DETECT_SIZE = 35f;
+    private const float Y_DETECT_SIZE = 25f;
+
+
+    private void Awake()
+    {
+        //Load components
+        player = GetComponentInParent<Player>();
+
+        zombie = GameObject.FindGameObjectWithTag(Settings.zombieTag);
+    }
+
+    private void Update()
+    {
+        //Check for near cars.
+        detectedCars = Physics2D.OverlapCircleAll(transform.position, Settings.carDetectRadius, Settings.carLayer);
+
+        //Check for near zombies
+        detectedZombies = Physics2D.OverlapBoxAll(transform.position, new Vector2(X_DETECT_SIZE, Y_DETECT_SIZE), 360, Settings.zombieLayer);
+
+
+        if (IsThereZombie())
+        {
+            //There are some zombies!!
+            ArrangeZombiesVisibilities();
+
+        }
+
+
+        //Check environment for a car and press F to ride that.
+        SetActiveCar();
+
+
+    }
+
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.CompareTag(Settings.zombieSpawnerTag))
+        {
+            //Spawn Zombiessss !!!
+            collision.gameObject.GetComponent<ZombieSpawnEvent>().CallZombieSpawnEvent();
+
+        }
+    }
+
+
+    //<summary>
+    //Set the active car
+    //</summary>
+    private void SetActiveCar()
+    {
+        //Button for interacting with cars
+        bool interactWithCar = Input.GetKeyDown(KeyCode.F);
+
+        if (interactWithCar && player.activeCar.activeCar == null)
+        {
+            //If there is any car then drive it.
+            if (IsThereCar())
+            {
+                //Get closest car near to player
+                GameObject closestCar = UtilsClass.GetClosestGameobject(detectedCars);
+                //Set nearest car as current car.
+                player.setActiveCarEvent.CallSetActiveCar(closestCar);
+
+            }
+
+        }
+        //If player already rides a car and if player press F then player leaves the car.
+        else if (interactWithCar && player.activeCar.activeCar != null)
+        {
+            //Call deactive car event
+            player.setActiveCarEvent.CallDeactiveCar();
+        }
+    }
+
+    //<summary>
+    //Arrange zombies visibilities based on the character's look angle
+    //<summary>
+    private void ArrangeZombiesVisibilities()
+    {
+        foreach (Collider2D collider in detectedZombies)
+        {
+            RaycastHit2D hit = Physics2D.Raycast(transform.position, collider.gameObject.transform.position - transform.position, Mathf.Infinity, collidableLayers);
+
+
+            if (hit.collider.gameObject != collider.gameObject && !hit.collider.gameObject.CompareTag(Settings.zombieTag))
+            {
+                ZombieInvisibleProcess(collider.gameObject);
+            }
+            else
+            {
+
+                ZombieVisibleProcess(collider.gameObject);
+            }
+
+
+        }
+    }
+
+    //<summary>
+    //Turn zombies invisible objects when character doesnt see them
+    //<summary>
+    private void ZombieInvisibleProcess(GameObject gameobject)
+    {
+        ZombieController zombieController = gameobject.gameObject.GetComponent<ZombieController>();
+        zombieController.ZombieInvisibleProcess();
+
+    }
+    //<summary>
+    //Turn zombies visible objects when character does see them
+    //</summary>
+    private void ZombieVisibleProcess(GameObject gameObject)
+    {
+        ZombieController zombieController = gameObject.gameObject.GetComponent<ZombieController>();
+        zombieController.ZombieVisibleProcess();
+    }
+
+
+
+    //<summary>
+    //Check if there any car close to us.
+    //</summary>
+    public bool IsThereCar()
+    {
+        return detectedCars.Length > 0;
+    }
+
+    //<summary>
+    //Check if there any zombie close to us.
+    //</summary>
+    private bool IsThereZombie()
+    {
+        return detectedZombies.Length > 0;
+    }
+
+
+}
