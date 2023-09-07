993c729eb953e804614dcbcdca129fab7ee67f91
KeremYanlic <keremyanlc@outlook.com>
2023-09-08 00:39:58 +0300

    MovementToPositionEvent script file has been updated

---
+++ Movement/MovementToPositionEvent.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+public class MovementToPositionEvent : MonoBehaviour
+{
+    public event Action<MovementToPositionEvent, MovementToPositionEventArgs> OnMovementToPosition;
+
+    public void CallMovementToPosition(Vector3 movePosition, Vector3 currentPosition, float moveSpeed, Vector2 moveDirection, bool isRolling)
+    {
+        OnMovementToPosition?.Invoke(this, new MovementToPositionEventArgs()
+        {
+            movePosition = movePosition,
+            currentPosition = currentPosition,
+            moveSpeed = moveSpeed,
+            moveDirection = moveDirection,
+            isRolling = isRolling
+        });
+    }
+}
+public class MovementToPositionEventArgs : EventArgs
+{
+    public Vector3 movePosition;
+    public Vector3 currentPosition;
+    public float moveSpeed;
+    public Vector2 moveDirection;
+    public bool isRolling;
+}
