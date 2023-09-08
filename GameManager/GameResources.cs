feaccc942823378965875944d4f5c527bc0cf839
KeremYanlic <keremyanlc@outlook.com>
2023-09-08 22:05:21 +0300

    GameResources script file has been updated

---
+++ GameManager/GameResources.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameResources : SingletonMonobehaviour<GameResources>
+{
+    public PlayerDetailsSO playerDetailsSO;
+
+
+    //Ammo icon for the Weapon Status UI
+    public GameObject ammoIconPrefab;
+
+
+}
