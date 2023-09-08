63601a6abe2ba22b1ccf3d2990a2db8b82191f48
KeremYanlic <keremyanlc@outlook.com>
2023-09-08 21:53:50 +0300

    Enum script file has been added

---
+++ Misc/Enum.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public enum AimDirection
+{
+    Up,
+    UpRight,
+    Right,
+    Down,
+    Left,
+    UpLeft
+}
+
+public enum ZombieEvents
+{
+    IdleAtStart,
+    Idle,
+    Patrolling,
+    Chase
+
+}
