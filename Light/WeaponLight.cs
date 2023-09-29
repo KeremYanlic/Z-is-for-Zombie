616022041a377c7a578a78b3882c1ccba5572fee
KeremYanlic <keremyanlc@outlook.com>
2023-09-29 18:07:52 +0300

    WeaponLight script has been updated

--- Light/WeaponLight.cs
+++ Light/WeaponLight.cs
@@ -6,32 +6,57 @@ using UnityEngine.Rendering.Universal;
 public class WeaponLight : MonoBehaviour
 {
     private AimThroughSightEvent aimThroughSightEvent;
+    private DisableWeaponEvent disableWeaponEvent;
+    private SetActiveWeaponEvent setActiveWeaponEvent;
 
     private Light2D weaponLightRight;
     private Light2D weaponLightLeft;
+    private bool isWeaponActive = true;
     private bool isOpen = true;
     private void Awake()
     {
+        //Load components
         aimThroughSightEvent = gameObject.GetComponentInParent<Player>().GetComponent<AimThroughSightEvent>();
+        disableWeaponEvent = gameObject.GetComponentInParent<Player>().GetComponent<DisableWeaponEvent>();
+        setActiveWeaponEvent = gameObject.GetComponentInParent<Player>().GetComponent<SetActiveWeaponEvent>();
 
         weaponLightRight = transform.GetChild(0).GetComponent<Light2D>();
         weaponLightLeft = transform.GetChild(1).GetComponent<Light2D>();
     }
-    // Start is called before the first frame update
-    void Start()
-    {
-
-    }
+   
     private void OnEnable()
     {
         //Subscribe to aim through sight event
         aimThroughSightEvent.OnAimThroughSight += AimThroughSightEvent_OnAimThroughSight;
+
+        //Subscribe to disable weapon event
+        disableWeaponEvent.OnDisableWeapon += DisableWeaponEvent_OnDisableWeapon;
+
+        //Subscribe to enable weapon event
+        setActiveWeaponEvent.OnEnableWeapon += SetActiveWeaponEvent_OnEnableWeapon;
     }
+
+   
     private void OnDisable()
     {
         //Unubscribe from aim through sight event
         aimThroughSightEvent.OnAimThroughSight -= AimThroughSightEvent_OnAimThroughSight;
+
+        //Unubscribe from disable weapon event
+        disableWeaponEvent.OnDisableWeapon -= DisableWeaponEvent_OnDisableWeapon;
+
+        //Unsubscribe from enable weapon event
+        setActiveWeaponEvent.OnEnableWeapon -= SetActiveWeaponEvent_OnEnableWeapon;
     }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!isWeaponActive) return;
+        ManageWeaponLight();
+    }
+
 
     private void AimThroughSightEvent_OnAimThroughSight(AimThroughSightEvent arg1, AimThrouhSightEventArgs aimThrouhSightEventArgs)
     {
@@ -41,11 +66,26 @@ public class WeaponLight : MonoBehaviour
         }
     }
 
-    // Update is called once per frame
-    void Update()
+    private void DisableWeaponEvent_OnDisableWeapon(DisableWeaponEvent obj)
     {
-        ManageWeaponLight();
+        isWeaponActive = false;
+        if (isOpen)
+        {
+            CloseWeaponLights();
+        }
     }
+
+    private void SetActiveWeaponEvent_OnEnableWeapon(SetActiveWeaponEvent obj)
+    {
+        isWeaponActive = true;
+        if (!isOpen)
+        {
+            OpenWeaponLights();
+        }
+    }
+
+
+  
 
     //<summary>
     //Manage the weapon's ligth.
@@ -57,7 +97,7 @@ public class WeaponLight : MonoBehaviour
             OpenCloseLight();
         }
     }
-
+   
     private void OpenCloseLight()
     {
         isOpen = !isOpen;
@@ -74,4 +114,14 @@ public class WeaponLight : MonoBehaviour
             weaponLightLeft.enabled = isOpen;
         }
     }
+    private void OpenWeaponLights()
+    {
+        if (!isOpen)
+        {
+            isOpen = true;
+            weaponLightRight.enabled = isOpen;
+            weaponLightLeft.enabled = isOpen;
+        }
+    }
+    
 }
