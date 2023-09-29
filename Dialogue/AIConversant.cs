c6d020b46e717facdc12ba04c81184f2a3b25eb6
KeremYanlic <keremyanlc@outlook.com>
2023-09-29 18:03:52 +0300

    AIConversant script file has been added

--- Dialogue/AIConversant.cs
+++ Dialogue/AIConversant.cs
@@ -2,7 +2,24 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class AIConversant : MonoBehaviour
+public class AIConversant : MonoBehaviour,IDialogueable
 {
-    public DialogueSO dialogueSO;
+    [SerializeField] private DialogueSO dialogueSO;
+    [SerializeField] private AIDialogueConfigurationSO aIDialogueConfigurationSO;
+    [SerializeField] private Transform playerTargetPosition;
+    public DialogueSO GetDialogueSO()
+    {
+        return dialogueSO;
+    }
+    public AIDialogueConfigurationSO GetDialogueConfigurationSO()
+    {
+        return aIDialogueConfigurationSO;
+    }
+
+    public Vector3 GetDialoguePosition()
+    {
+        return playerTargetPosition.position;
+    }
+
+   
 }
