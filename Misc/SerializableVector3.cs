513121303a2d2ed696e67c148c4c26cec5a64fb7
KeremYanlic <keremyanlc@outlook.com>
2023-10-08 20:36:18 +0300

    SerializableVector3 script file has been added and updated

---
+++ Misc/SerializableVector3.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[System.Serializable]
+public class SerializableVector3
+{
+    private float x, y, z;
+
+    // <summary>
+    // Copy over the state from an existing Vector3.
+    // </summary>
+    public SerializableVector3(Vector3 vector)
+    {
+        x = vector.x;
+        y = vector.y;
+        z = vector.z;
+    }
+
+    // <summary>
+    // Create a Vector3 from this class' state.
+    // </summary>
+    public Vector3 ToVector()
+    {
+        return new Vector3(x, y, z);
+    }
+}
