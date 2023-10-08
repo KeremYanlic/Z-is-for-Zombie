9ba77aa2a58a415c465de6d7e093abe1a621557b
KeremYanlic <keremyanlc@outlook.com>
2023-10-08 20:35:11 +0300

    EquipLocation enum has been added to Enum script file

--- Misc/Enum.cs
+++ Misc/Enum.cs
@@ -2,21 +2,30 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public enum AimDirection
+public enum AimDirection 
 {
-    Up,
-    UpRight,
-    Right,
-    Down,
-    Left,
-    UpLeft
+   Up,
+   UpRight,
+   Right,
+   Down,
+   Left,
+   UpLeft
 }
 
 public enum ZombieEvents
 {
-    IdleAtStart,
-    Idle,
-    Patrolling,
-    Chase
+   IdleAtStart,
+   Idle,
+   Patrolling,
+   Chase
 
+}
+public enum EquipLocation
+{
+    Helmet,
+    HeadLamp,
+    Shield,
+    Bag,
+    FirstGun,
+    SecondGun,
 }
