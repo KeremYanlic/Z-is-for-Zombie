0ecbcae6f371f4dee3b89b54d7c5786be0a8a6c7
KeremYanlic <keremyanlc@outlook.com>
2023-12-23 16:53:19 +0300

    Inventory script has been updated

--- Inventory/Inventory.cs
+++ Inventory/Inventory.cs
@@ -1,219 +1,48 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using System;
 
-// <summary>
-// Provides storage for the player inventory. A configurable number of slots are available.
-// </summary>
-public class Inventory : SingletonMonobehaviour<Inventory> , ISaveable
+public abstract class Inventory : MonoBehaviour
 {
-    public event Action<Inventory> OnInventoryUpdated;
-
-    // CONFIG STATE
     #region Tooltip
-    [Tooltip("Allowed size")]
+    [Tooltip("Allowed inventory size")]
     #endregion
-    [SerializeField] private int inventorySize = 16;
-
+    [SerializeField] protected int inventorySize = 48;
     public struct InventorySlot
     {
-        public InventoryItemSO item;
+        public InventoryItemSO itemSO;
         public int number;
     }
-    private InventorySlot[] slots;
-
-    protected override void Awake()
+    public struct InventorySlotRecord
     {
-        base.Awake();
-
-        slots = new InventorySlot[inventorySize];
+        public string itemID;
+        public int number;
     }
-    // <summary>
-    // Attempt to add the items to the first available slot
-    // </summary>
-    public bool AddToFirstEmptySlot(InventoryItemSO item,int number)
+  
+    public abstract bool AddToFirstEmptySlot(InventoryItemSO itemSO, int number);
+    public abstract bool AddItemToSlot(int slot, InventoryItemSO itemSO, int number);
+    public abstract void RemoveFromSlot(int slot, int number);
+
+    public abstract bool HasItem(InventoryItemSO itemSO);
+
+    public abstract InventoryItemSO GetItemInSlot(int slot);
+    public abstract int GetNumberInSlot(int slot);
+
+    public abstract int FindStack(InventoryItemSO itemSO);
+    public abstract int FindEmptySlot();
+    public abstract int GetInventorySize();
+    public virtual bool HasSpaceFor(InventoryItemSO itemSO)
     {
-        int i = FindSlot(item);
-
-        if(i < 0)
-        {
-            return false;
-        }
-        slots[i].item = item;
-        slots[i].number += number;
-
-        OnInventoryUpdated?.Invoke(this);
-        return true;
-
+        return FindSlot(itemSO) >= 0;
     }
-
-    // <summary>
-    // Remove a number of items from the given slot. Will never remove more that there are.
-    // </summary>
-    public void RemoveFromSlot(int slot,int number)
+    
+    protected int FindSlot(InventoryItemSO itemSO)
     {
-        slots[slot].number -= number;
-        if(slots[slot].number <= 0)
-        {
-            slots[slot].number = 0;
-            slots[slot].item = null;
-        }
-        OnInventoryUpdated?.Invoke(this);
-    }
-    // <summary>
-    // Will add an item to the given slot if possible. If there is already
-    // a stack of this type , it will add to the existing stack. Otherwise,
-    // it will be added to the first empty slot.
-    // </summary>
-    public bool AddItemToSlot(int slot,InventoryItemSO item,int number)
-    {
-        if(slots[slot].item != null)
-        {
-            return AddToFirstEmptySlot(item, number);
-        }
-        var i = FindStack(item);
-        if(i >= 0)
-        {
-            slot = i;
-        }
-        slots[slot].item = item;
-        slots[slot].number += number;
-        OnInventoryUpdated?.Invoke(this);
-
-        return true;
-    }
-
-    // <summary>
-    // Is there an instance of the item in the inventory ?
-    // </summary>
-    public bool HasItem(InventoryItemSO item)
-    {
-        for(int i = 0; i< slots.Length; i++)
-        {
-            if (object.ReferenceEquals(slots[i].item, item))
-            {
-                return true;
-            }
-        }
-        return false;
-    }
-
-    // <summary>
-    // Return the item type in the given slot.
-    // </summary>
-    public InventoryItemSO GetItemInSlot(int slot)
-    {
-        return slots[slot].item;
-    }
-
-    // <summary>
-    // Get the number of items in the given slot
-    // </summary>
-    public int GetNumberInSlot(int slot)
-    {
-        return slots[slot].number;
-    }
-
-    // <summary>
-    // Could this item fit anywhere in the inventory
-    // </summary>
-    public bool HasSpaceFor(InventoryItemSO item)
-    {
-        return FindSlot(item) >= 0;
-    }
-
-
-    // <summary>
-    // Find a slot that can accomodate the given item. Returns -1 if no slot is found.
-    // </summary>
-    private int FindSlot(InventoryItemSO item)
-    {
-        int i = FindStack(item);
-        if(i < 0)
+        int i = FindStack(itemSO);
+        if (i < 0)
         {
             i = FindEmptySlot();
         }
         return i;
     }
-
-
-    // <summary>
-    // Find an empty slot.Returns -1 if all slots are full.
-    // </summary>
-    private int FindEmptySlot()
-    {
-        for(int i = 0; i <slots.Length; i++)
-        {
-            if(slots[i].item == null)
-            {
-                return i;
-            }
-        }
-        return -1;
-    }
-
-
-    // <summary>
-    // Find an existing stack of this item type. Returns -1 if no stack exists or if the item is not stackable
-    // </summary>
-    private int FindStack(InventoryItemSO item)
-    {
-        if (!item.IsStackable())
-        {
-            return -1;
-        }
-
-        for(int i = 0; i < slots.Length; i++)
-        {
-            if (object.ReferenceEquals(slots[i].item, item))
-            {
-                return i;
-            }
-        }
-        return -1;
-    }
-
-    // <summary>
-    // How many slots are in the inventory ?
-    // </summary>
-    public int GetSize()
-    {
-        return slots.Length;
-    }
-
-    [System.Serializable]
-    private struct InventorySlotRecord
-    {
-        public string itemID;
-        public int number;
-    }
-
-
-    public object CaptureState()
-    {
-        var slotStrings = new InventorySlotRecord[inventorySize];
-        for(int i = 0; i < inventorySize; i++)
-        {
-            if(slots[i].item != null)
-            {
-                slotStrings[i].itemID = slots[i].item.GetItemID();
-                slotStrings[i].number = slots[i].number;
-            }
-        }
-        return slotStrings;
-    }
-
-    public void RestoreState(object state)
-    {
-        var slotStrings = (InventorySlotRecord[])state;
-        for(int i = 0; i< inventorySize; i++)
-        {
-            slots[i].item = InventoryItemSO.GetFromId(slotStrings[i].itemID);
-            slots[i].number = slotStrings[i].number;
-        }
-        OnInventoryUpdated?.Invoke(this);
-
-    }
-
 }
